3fc727d94779fdd0f3bc2ca9a9f48eb7c98c3eb6
bm4656 <newjin46@gmail.com>
2024-10-23 21:50:32 +0900

    feat: app.module에 typeorm 연동 위한 import 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './res/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        retryAttempts: 10, // 연결에 실패했을 경우, 재시도 횟수
+        type: 'mysql',
+        host: '127.0.0.1',
+        port: 3306,
+        database: 'basic',
+        username: 'root',
+        password: '0000',
+        entities: [path.join(__dirname, '/entities/**/*.entity.{js, ts}')],
+        synchronize: false,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
